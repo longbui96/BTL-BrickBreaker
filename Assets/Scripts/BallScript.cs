af9d7b6c514814d8d76a88fcda1731319923dc14
luunhathuynh <luunhathuynh@gmail.com>
2017-04-28 10:28:35 +0700

    Add files via upload

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float ballForce;
+    bool GameStarted = false;
+    
+
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1") && GameStarted == false)
+        {
+            transform.SetParent(null);
+            GameStarted = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector2(ballForce, ballForce));
+        }
+	}
+}
